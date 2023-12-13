cd70433916bce2ce6e08a549a3634102b3613712
Kelwin Ndrianasolo <sk8kelwin@gmail.com>
2023-12-14 03:48:00 +0800

    discord login logout finish guestbook api finished

---
+++ src/server/api/routers/guestbook.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+
+
+export const guestbookRouter = createTRPCRouter({
+    postMessage: protectedProcedure
+        .input(z.object({
+            name: z.string(),
+            message: z.string(),
+        })
+        )
+        .mutation(async ({ ctx, input }) => {
+            try {
+                await ctx.db.guestbook.create({
+                    data: {
+                        name: input.name,
+                        message: input.name,
+                    },
+                });
+            } catch (error) {
+                console.log("error", error);
+            };
+        }),
+    getAll: publicProcedure.query(async({ctx})=>{
+        try{
+            return await ctx.db.guestbook.findMany({
+                select:{
+                    name:true,
+                    message:true,
+                },
+                orderBy:{
+                    createdAt:"desc"
+                }
+            })
+        }catch(error){
+            console.log("error",error);
+        }
+    })
+
+});
+
